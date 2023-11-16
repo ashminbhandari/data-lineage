806c7b7fbe6eced560fb0ba10813f4af0c2bf173
Adefioye <abdulhakeemadefioye@gmail.com>
2023-11-16 01:28:39 -0600

    Add table markup

--- web/src/components/TableView.js
+++ web/src/components/TableView.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import Table from "react-bootstrap/Table";
 
 const TableView = () => {
+  const COLUMN_NAMES = ["first name", "last name", "User name"];
+  const TABLE_DATA = [
+    {
+      firstName: "Mark",
+      lastName: "Shafa",
+      userName: "Balikis",
+    },
+    {
+      firstName: "Mark",
+      lastName: "Shafa",
+      userName: "Balikis",
+    },
+    {
+      firstName: "Mark",
+      lastName: "Shafa",
+      userName: "Balikis",
+    },
+  ];
   return (
-    <div>TableView</div>
-  )
-}
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          {COLUMN_NAMES.map((columnName) => (
+            <th key={columnName}>First Name</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+          {TABLE_DATA.map((tableData) => (
+            <tr key={tableData.firstName}>
+              <td>{tableData.firstName}</td>
+              <td>{tableData.lastName}</td>
+              <td>{tableData.userName}</td>
+            </tr>
+          ))}
+      </tbody>
+    </Table>
+  );
+};
 
-export default TableView
+export default TableView;
